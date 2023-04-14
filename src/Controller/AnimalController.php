9053ed28af48f206c6aa7b60a3a7982e605606d9
FranMagalhaes <fmagalhaess23@gmail.com>
2023-04-14 11:02:48 -0300

    Criação de tela de Listagem de animais

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -4,41 +4,43 @@ namespace App\Controller;
 
 use App\Entity\Animal;
 use App\Form\AnimalType;
+use App\Repository\AnimalRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class AnimalController extends AbstractController
 {
 
-    public function index(EntityManagerInterface $em) : Response
+    public function index(AnimalRepository $animalRepository) : Response
     {
+        //buscar no bd todos os animais cadastrados
+        $data['animais'] = $animalRepository->findAll();
+        $data['titulo'] = 'Gerenciar Animais';
+
+        return $this->render('animal/index.html.twig', $data);
+    }
+
+    public function adicionar(Request $request, EntityManagerInterface $em) : Response
+    {
+        $msg = '';
         $animal = new Animal();
-        $animal -> setQntdLeite(123);
-        $animal -> setPeso(123);
-        $animal -> setQntdRacao(123);
-        $animal -> setDtNasc(new DateTimeImmutable('2020-01-01'));
-        
-        
-    try{
-        $em -> persist($animal);
-        $em -> flush();
-        $msg = "Animal cadastrado com sucesso";
-    }
+        $form = $this->createForm(AnimalType::class, $animal);
+        $form->handleRequest($request);
 
-    catch(Exception $e){
-        $msg = "Erro ao cadastrar animal";
+        if($form->isSubmitted() && $form->isValid()){
+            //Salvar o novo Animal no bd
+            $em->persist($animal); //salvar na memoria
+            $em->flush();
+            $msg = "Animal cadatrado com sucesso!";
         }
-        return new Response("<h1>" . $msg . "</h1>");
-    }
 
-    public function adicionar() : Response
-    {
-        $form = $this->createForm(AnimalType::class);
         $data['titulo'] = 'Cadastrar novo animal';
         $data['form'] = $form;
+        $data['msg'] = $msg;
 
         return $this->renderform('animal/form.html.twig', $data);
     }
